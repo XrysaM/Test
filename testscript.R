a4d5c5633c007e4cbd79226b8a7a4e2b1a56a058
xrysa_m <xrysa.m99@gmail.com>
2022-07-28 15:57:17 +0300

    add testscript.R -> What i'm going to test sites- name, what it analyzes

--- testscript.R
+++ testscript.R
@@ -1,1 +1,11 @@
 #this is a test script on my test project
+
+#Rpubs - Classification with Decision Trees
+#C4.5 - C5.0
+
+#Machine learning mastery - Non-Linear Classification in R with Decision Trees
+#Cart - C4.5 - Bagging Cart - Random forest - Boosted C5.0 and more
+
+#Datacamp - R Decision Trees Tutorial 
+#Classification trees - Random Forests - Boosting
+
